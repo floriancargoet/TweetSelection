f6e10312fd4547934e7affbe100f87582644f397
Florian Cargoët <florian.cargoet+github@centrale-marseille.fr>
2012-09-21 10:01:22 +0200

    added comment about kdialog

--- tweetselection.sh
+++ tweetselection.sh
@@ -5,6 +5,7 @@
 # select something, hit your shortcut, confirm and it's done!
 
 content=$(xclip -o)
+# if you are on KDE, use 'kdialog' instead of 'zenity'
 if zenity --question --text "Tweet this: \n\"$content\"" ; then
     t update "$content"
 fi
